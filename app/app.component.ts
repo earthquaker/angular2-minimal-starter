ca257d0d4cb6423d287bff30a65f6152b046265a
earthquaker <pe_lias@msn.com>
2016-03-07 23:30:17 +0100

    added pages and router upd

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,22 @@
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {CrisisListComponent}   from './crisis-list.component';
+import {HeroListComponent}     from './hero-list.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App!</h1>'
+    template: `
+    <h1>Component Router</h1>
+    <nav>
+      <a [routerLink]="['CrisisCenter']">Crisis Center</a>
+      <a [routerLink]="['Heroes']">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+    directives: [ROUTER_DIRECTIVES]
 })
-export class AppComponent {}
+@RouteConfig([
+    {path:'/crisis-center', name: 'CrisisCenter', component: CrisisListComponent},
+    {path:'/heroes',        name: 'Heroes',       component: HeroListComponent}
+])
+export class AppComponent { }
