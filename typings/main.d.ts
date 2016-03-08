bba2dff96b1717a3bdf489a07b082026f2cefb05
Elias Hansen <pe_lias@msn.com>
2016-03-08 16:22:39 +0100

    upd

--- typings/main.d.ts
+++ typings/main.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="main\ambient\es6-shim\index.d.ts" />
 /// <reference path="main\ambient\jasmine\index.d.ts" />
 /// <reference path="main\ambient\jasmine\jasmine.d.ts" />
+/// <reference path="main\definitions\moment\index.d.ts" />
